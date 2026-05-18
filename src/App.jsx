5b02b0495d5137682c2f6732fbd4c1193bddb51e
Annie <anniehastoriya@gmail.com>
2026-05-17 20:54:52 -0500

    Submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
+// state
+// shows - in shows it holds the full list of tv shows. its loaded from from the tvShows data.
+//selectedShow - this tracks whichever show the user has currently picked.
+
 import { useState } from "react";
-import { tvShows } from "./shows/dat";
-import ShowDetails from "/shows/ShowDetails";
-import ShowSelection from "./shows/ShowSeletion";
+import { tvShows } from "./shows/data";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
 
-/**
- * React TV is an web streaming platform that allows users to browse
- * through the episodes of a variety of different shows.
- */
 export default function App() {
-  const [shows] = useStae(tvShows);
+  const [shows] = useState(tvShows);
   const [selectedShow, setSelectedShow] = useState();
-
+  // shows the title app with the <header></>header> element.
+  //ShowSelction is the list where the user will pick a show.
+  // <main> has the ShowDetails which will display infomation about the selected show.
   return (
     <>
       <header>
         <p>React TV</p>
         <ShowSelection
-          show={shows}
+          shows={shows}
           selectedShow={selectedShow}
-          setSelectedShow={setSelctedShow}
+          setSelectedShow={setSelectedShow}
         />
       </header>
+
       <main>
-        <ShowDetails key={selectedshow?.name} show={selectedShow} />
+        <ShowDetails key={selectedShow?.name} show={selectedShow} />
       </main>
     </>
   );
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,25 +1,27 @@
-import "./shows.css";
 import { useState } from "react";
-import EpisodeList from "../epsisodes.EpisodeList";
-import EpisodeDetails from "../episode/EpisodeDetails";
+
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+
+import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails({ show }) {
   const [selectedEpisode, setSelectedEpisode] = useState();
 
   if (!show) {
-    return <p> className="show-details "Please select a show</p>;
+    return <p>Please select a show to learn more.</p>;
   }
+
   return (
-  <div className="show-details">
-    <EpisodeList
-    name={show.name}
-    episodes={show.episodes}
-    selectedEpisode={selectedEpisode}
-    setSelectedEpisode={setSelectedEpisode}
-    />
-    <EpisodeDetails episode= {setSelectedEpisode} />
-    
-  </div>;
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
   );
 }
